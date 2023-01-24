64129a2c41956101e0e80f8a31e72424a2d8821c
dlatldud11 <ssh456852@naver.com>
2023-01-24 22:21:02 +0900

    프로젝트 생성

---
+++ VacationCalender/src/main/java/com/vc/pkg/VacationCalenderApplication.java
@@ -0,0 +1,13 @@
+package com.vc.pkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationCalenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationCalenderApplication.class, args);
+	}
+
+}
---
+++ VacationCalender/src/test/java/com/vc/pkg/VacationCalenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vc.pkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationCalenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
