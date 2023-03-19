1e2953826f77bab6cd3728403510c6c6d3c89783
dlatldud11 <ssh456852@naver.com>
2023-03-19 23:47:09 +0900

    initial commit

---
+++ bestvacation/src/main/java/com/project/bestvacation/BestvacationApplication.java
@@ -0,0 +1,14 @@
+package com.project.bestvacation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestvacationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestvacationApplication.class, args);
+		System.out.println("mainpage");
+	}
+
+}
