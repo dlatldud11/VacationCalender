60459d2e00348be63c2c3bacfab73ec64913b6d0
dlatldud11 <ssh456852@naver.com>
2023-03-26 12:51:45 +0900

    react 부트스트랩 템플릿 적용 테스트

--- bestvacation/src/main/bestvaction-app/src/App.js
+++ bestvacation/src/main/bestvaction-app/src/App.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
    const [hello, setHello] = useState('')
@@ -13,6 +14,7 @@ function App() {
     return (
         <div>
             백엔드에서 가져온 데이터입니다 : {hello}
+            <button class="btn btn-default" type="submit">Button</button>
         </div>
     );
 }
