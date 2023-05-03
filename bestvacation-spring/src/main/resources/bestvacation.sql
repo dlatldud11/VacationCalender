5ac523fbc55aae7c0899df73c31a3b587c5ccc3d
임시영 <ssh456852@naver.com>
2023-05-03 17:57:01 +0900

    테이블 생성 쿼리문 DDL 수정

--- bestvacation-spring/src/main/resources/bestvacation.sql
+++ bestvacation-spring/src/main/resources/bestvacation.sql
@@ -1,37 +1,30 @@
-﻿CREATE TABLE `member` (
-	`m_key`	VARCHAR(255)	NOT NULL,
-	`m_id`	VARCHAR(255)	NULL,
-	`m_password`	VARCHAR(255)	NULL,
-	`m_join_ymd`	VARCHAR(255)	NULL,
-	`m_year_vac_cnt`	VARCHAR(255)	NULL,
-	`m_used_vac_cnt`	VARCHAR(255)	NULL,
-	`m_total_vac_cnt`	VARCHAR(255)	NULL,
-	`m_sign_up_ymd`	VARCHAR(255)	NULL,
-	`m_state`	VARCHAR(255)	NULL,
-	`m_email`	VARCHAR(255)	NULL,
-	`m_name`	VARCHAR(255)	NULL,
-	`m_gender`	VARCHAR(255)	NULL,
-	`m_birthday`	VARCHAR(255)	NULL,
-	`m_sys_time`	VARCHAR(255)	NULL,
-	`m_sys_user`	VARCHAR(255)	NULL
-);
+﻿CREATE TABLE member (
+	m_key	int	NOT null auto_increment,
+	m_id	VARCHAR(255)	NULL,
+	m_password	VARCHAR(255)	NULL,
+	m_join_ymd	VARCHAR(8)	NULL,
+	m_year_vac_cnt	int	NULL,
+	m_used_vac_cnt	int	NULL,
+	m_total_vac_cnt	int	NULL,
+	m_sign_up_ymd	VARCHAR(8)	NULL,
+	m_state	VARCHAR(2)	NULL,
+	m_email	VARCHAR(255)	NULL,
+	m_name	VARCHAR(255)	NULL,
+	m_gender	VARCHAR(2)	NULL,
+	m_birthday	VARCHAR(8)	NULL,
+	m_sys_time	timestamp	NULL,
+	m_sys_user	VARCHAR(255)	null,
+	primary key(m_key)
+)ENGINE=MYISAM CHARSET=utf8;
 
-CREATE TABLE `calender` (
-	`c_key`	VARCHAR(255)	NOT NULL,
-	`c_ymd`	VARCHAR(255)	NULL,
-	`m_key`	VARCHAR(255)	NOT NULL,
-	`c_type`	VARCHAR(255)	NULL,
-	`c_cncl_yn`	varchar	NULL,
-	`c_aprov_yn`	VARCHAR(255)	NULL,
-	`c_sys_time`	VARCHAR(255)	NULL,
-	`c_sys_user`	VARCHAR(255)	NULL
-);
-
-ALTER TABLE `member` ADD CONSTRAINT `PK_MEMBER` PRIMARY KEY (
-	`m_key`
-);
-
-ALTER TABLE `calender` ADD CONSTRAINT `PK_CALENDER` PRIMARY KEY (
-	`c_key`
-);
-
+CREATE TABLE calender(
+	c_key	int	NOT null auto_increment,
+	c_ymd	VARCHAR(8)	NULL,
+	m_key	int	NOT NULL,
+	c_type	VARCHAR(2)	NULL,
+	c_cncl_yn	varchar(2)	NULL,
+	c_aprov_yn	VARCHAR(2)	NULL,
+	c_sys_time	timestamp	NULL,
+	c_sys_user	VARCHAR(255)	null,
+	primary key(c_key)
+)ENGINE=MYISAM CHARSET=utf8;
